8cf2dc44409ee720496d50dbad982278b9918cde
V1n-T3ch <vincegichomo704@gmail.com>
2025-05-15 16:01:35 +0300

    First try on seo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,26 +6,38 @@ import MensGrid from "../components/MensGrid.jsx";
 import BestSellerGrid from "../components/BestSellerGrid.jsx";
 import LadiesGrid from "../components/LadiesGrid.jsx";
 import Footer from "../components/Footer.jsx";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div className="home-page">
-      <Header />
-      <Banner />
-      <Categories />
-      {/* Added different categorised grids */}
-      <BestSellerGrid />
-      <LadiesGrid />
-      <MensGrid />
+    <>
+      <Helmet>
+        <title>Loris | Home</title>
+        <meta name="description" content="Discover the latest trends and styles at Loris Kenya." />
+        <meta name="keywords" content="loris kenya, loris kenya home, loris kenya shop, loris perfume, loris kenya perfume" />
+        <link rel="canonical" href="https://www.loriskenya.com/" />
+        <meta property="og:title" content="Loris | Home" />
+        <meta property="og:description" content="Discover the latest trends and styles at Loris Kenya." />
+        <meta property="og:url" content="https://www.loriskenya.com/" />
+      </Helmet>
+      <div className="home-page">
+        <Header />
+        <Banner />
+        <Categories />
+        {/* Added different categorised grids */}
+        <BestSellerGrid />
+        <LadiesGrid />
+        <MensGrid />
 
-      <Link to="/all-products">
-        <button style={{ textDecoration: "none" }} className="view-all-button">
-          View All Products
-        </button>
-      </Link>
+        <Link to="/all-products">
+          <button style={{ textDecoration: "none" }} className="view-all-button">
+            View All Products
+          </button>
+        </Link>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
